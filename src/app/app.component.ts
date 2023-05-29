2a69d68f364b3dc1cfba227d502876c4b11f60c1
iman bayat mokhtari <105519831+imanbayat8989@users.noreply.github.com>
2023-05-29 16:27:44 +0430

    Step 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  users:any;
   title = 'DatingAppAngulars';
+
+  constructor(private http:HttpClient) {
+
+  }
+
+  ngOnInit(): void {
+      this.getusers();
+  }
+  
+  getusers(){
+    this.http.get("https://localhost:7097/api/user").subscribe(x=>{
+      this.users=x;
+    }
+    )
+
+  }
 }
